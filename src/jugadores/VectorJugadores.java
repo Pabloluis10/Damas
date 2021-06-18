f67ffd3811ddd5a61cca058936689375ea3fe67e
Pabloluis Toyom <pabloluistoyom201830896@cunoc.edu.gt>
2021-06-18 11:18:11 -0600

    Agregue VectorJugador.java para administrar a los jugadore

---
+++ src/jugadores/VectorJugadores.java
@@ -0,0 +1,48 @@
+package jugadores;
+
+import java.util.Scanner;
+
+public class VectorJugadores {
+    //atributos
+    private Jugador [] jugadores = new Jugador[5];
+    private int posLibre;
+    private Scanner entrada = new Scanner(System.in);
+
+    public VectorJugadores() {
+        posLibre=0;
+    }
+
+    public void ingresarJugador(){
+        System.out.println("\n\n==================== Ingresar Jugador ====================");
+        System.out.print("Ingrese el nombre del jugador");
+        String nombre = entrada.nextLine();
+        jugadores[posLibre] = new Jugador(nombre);
+    }
+
+    public Jugador seleccionarJugador1( ) {
+        System.out.println("\n==================== Seleccionar Jugador 1 ====================");
+        mostraJugadores(); 
+        System.out.print("Que jugador va a seliccionar (num): ");
+        int num = entrada.nextInt();
+        entrada.nextLine();
+        num--; //indice del jugador
+        return jugadores[num];
+    }
+
+    public Jugador seleccionarJugador2( ) {
+        System.out.println("\n==================== Seleccionar Jugador 2 ====================");
+        mostraJugadores();
+        System.out.print("Que jugador va a seliccionar (num): ");
+        int num = entrada.nextInt();
+        entrada.nextLine();
+        num--; //indice del jugador
+        return jugadores[num];
+    }
+
+    public void mostraJugadores(){
+        for (int i=0; i<posLibre; i++) {
+            System.out.println((i+1)+") "+jugadores[i].getNombre());
+        }
+    }
+    
+}

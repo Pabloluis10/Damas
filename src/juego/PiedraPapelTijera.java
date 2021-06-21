15edf78004879a10c584186706ea0a8b9eb7ff4f
Pabloluis Toyom <pabloluistoyom201830896@cunoc.edu.gt>
2021-06-20 23:13:41 -0600

    Agregue jugar() 7 duelo() en PiedraPapelTijera.java

--- src/juego/PiedraPapelTijera.java
+++ src/juego/PiedraPapelTijera.java
@@ -1,5 +1,7 @@
 package src.juego;
 
+import java.util.Random;
+
 import src.jugadores.Jugador;
 
 public class PiedraPapelTijera {
@@ -18,6 +20,45 @@ public class PiedraPapelTijera {
        System.out.println("Para seleccionar al jugador ellos tendran que jugar un duelo de ");
        System.out.println("¡Piedra Papel o Tijera!");
        System.out.println("El ganador sera el que incie la partida de Damas");
-
+       System.out.println("\nINICIA DUELO");
+       duelo(a, b);
    }
+
+   private void duelo(Jugador a, Jugador b){
+       final int Piedra=1;
+       final int papel=2;
+
+       int random1;
+       int random2;
+       do{
+           System.out.println("Piedra papel y tijera 1, 2, 3 ya");
+           random1 = (int)(Math.random()*2+1);
+           random2 = (int)(Math.random()*2+1);
+           if(random1 == Piedra){
+               System.out.println(a.getNombre()+": a elegido Piedra");
+           } else if(random1 == papel){
+               System.out.println(a.getNombre()+": a elegido Papel");
+           } else {
+               System.out.println(a.getNombre()+": a elegido Tijer");
+           }
+           if(random2 == Piedra){
+               System.out.println(b.getNombre()+": a elegido Piedra");
+           } else if(random2 == papel){
+               System.out.println(b.getNombre()+": a elegido Papel");
+           } else {
+               System.out.println(b.getNombre()+": a elegido Tijera");
+           }
+           if(random1 > random2){
+               System.out.println("El ganodor es: "+a.getNombre());
+               jugador1 = a;
+               jugador2 = b;
+           } else if(random2 > random1){
+               System.out.println("El ganador es: "+b.getNombre());
+               jugador1 = b;
+               jugador2 = a;
+           } else{
+               System.out.println("Empate.");
+           }
+       }while(random1 == random2);
+    }
 }
